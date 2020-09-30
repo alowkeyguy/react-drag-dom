514e1498c2c6a6b9fbb616d01619501df40f8323
张功涛 <zhanggt@tuya.com>
2020-09-30 17:39:54 +0800

    fix: fix

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { v4 } from 'uuid'
 
-import Dragger, {DragFrame, BatchAdd, BatchMove} from '@tuya-fe/react-drag-dom'
+import Dragger, {DragFrame, BatchAdd, BatchMove} from 'react-drag-dom'
 import './index.css'
 const bgImg = require('./img/bg.jpeg')
 
